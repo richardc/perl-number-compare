d4a35eed575470eb7ebb2cc6ab41cf0993695cd7
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2002-10-23 16:20:15 +0000

    basic test
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Number-Compare@834 c56e198b-7db7-0310-b3ab-8e406f684e33

--- Compare.pm
+++ Compare.pm
@@ -1,6 +1,41 @@
-# $Id: $
+# $Id$
 package Number::Compare;
 use strict;
-our $VERSION = '0.01';
+use Carp qw(croak);
+use vars qw/$VERSION/;
+$VERSION = '0.01';
+
+sub new  {
+    my $referent = shift;
+    my $class = ref $referent || $referent;
+    my $test = shift;
+
+    $test =~ m{^
+               ([<>]=?)?   # comparison
+               (.*?)       # value
+               ([kmg]i?)?  # magnitude
+              $}ix
+       or croak "don't understand '$test' as a test";
+
+    my $comparison = $1 || '==';
+    my $target     = $2;
+    my $magnitude  = $3;
+    $target *=           1000 if lc $magnitude eq 'k';
+    $target *=           1024 if lc $magnitude eq 'ki';
+    $target *=        1000000 if lc $magnitude eq 'm';
+    $target *=      1024*1024 if lc $magnitude eq 'mi';
+    $target *=     1000000000 if lc $magnitude eq 'g';
+    $target *= 1024*1024*1024 if lc $magnitude eq 'gi';
+
+    bless { target => $target, comparison => $comparison }, $class;
+}
+
+sub test {
+    my $self = shift;
+    my $value = shift;
+
+    my ($target, $comparison) = @$self{ 'target', 'comparison' };
+    return eval "$value $comparison $target";
+}
 
 1;
--- t/Number-Compare.t
+++ t/Number-Compare.t
@@ -1,5 +1,10 @@
 #!perl -w
 use strict;
-use Test::More tests => 1;
+use Test::More tests => 4;
 
 BEGIN { use_ok("Number::Compare") };
+
+my $gt_20 = Number::Compare->new('>20');
+ok(  $gt_20->test(21), ">20" );
+ok( !$gt_20->test(20) );
+ok( !$gt_20->test(19) );
